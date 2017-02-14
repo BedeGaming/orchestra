5ddf583a25dd231c82133fa8b669776854589fbd
Andrew Humphreys <ahumphreys87@googlemail.com>
2017-02-14 13:15:36 +0000

    Allow currency at end of string

--- src/helpers/currency.js
+++ src/helpers/currency.js
@@ -7,20 +7,27 @@ const channel = Radio.channel('global');
 
 class CurrencyHelper {
   addLocale(code, language) {
-    numeral.language(code, language);
+    numeral.register('locale', code, language);
   }
 
   format(amount, decimals = 0, currencyLocale = null) {
     const lang = currencyLocale || channel.request('currencyLocale');
 
     let formatStr = '$0,0';
+    let currencyEnd = false;
 
     if (lang) {
-      numeral.language(lang);
-      let language = numeral.languageData();
+      numeral.locale(lang);
+      let language = numeral.localeData();
+
       if (language.format) {
         formatStr = language.format;
       }
+    }
+
+    if (formatStr.charAt(formatStr.length - 1) === '$') {
+      currencyEnd = true;
+      formatStr = formatStr.slice(0, -1);
     }
 
     for (let i = 0; i < decimals; i++) {
@@ -29,6 +36,10 @@ class CurrencyHelper {
       }
 
       formatStr += '0';
+    }
+
+    if (currencyEnd) {
+      formatStr += '$';
     }
 
     return numeral(amount).format(formatStr);
