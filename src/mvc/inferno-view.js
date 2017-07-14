cb227a4d67b2a549e38bf0b99a8d20df04715898
Andrew Humphreys <ahumphreys87@googlemail.com>
2017-07-14 17:07:20 +0100

    Add infernoView

---
+++ src/mvc/inferno-view.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import { View } from 'backbone.marionette';
+
+const InfernoView = Marionette.View.extend({
+  _renderTemplate() {
+    const template = this.getTemplate();
+
+    // Allow template-less views
+    if (template === false) {
+      deprecate('template:false is deprecated.  Use _.noop.');
+      return;
+    }
+
+    // Add in entity data and template context
+    const data = this.mixinTemplateContext(this.serializeData());
+
+    // Render and add to el
+    const html = this._renderHtml(template, data);
+
+    if (html) {
+      this.attachElContent(html);
+    }
+  }
+});
+
+export default InfernoView;
